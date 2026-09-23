cbd069260a672f6969d077faa73f4109bd444555
eric <26416387.re@gmail.com>
2026-09-23 14:05:12 +0800

    Fix

--- nuxt-site/nuxt.config.ts
+++ nuxt-site/nuxt.config.ts
@@ -36,7 +36,7 @@ export default defineNuxtConfig({
     },
   },
   runtimeConfig: {
-    previewPassword: '520VANLAN',
+    previewPassword: '520VANLAN@2026',
     public: {
       googleMapsApiKey: '',
       franchiseApplicationEndpoint: '',
